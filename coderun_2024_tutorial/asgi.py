96dda1ebe14ee04de99cf7f07292a369899ae8bb
imimali <imimali18@gmail.com>
2024-11-10 21:56:15 +0200

    Initialise project

---
+++ coderun_2024_tutorial/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for coderun_2024_tutorial project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coderun_2024_tutorial.settings')
+
+application = get_asgi_application()
