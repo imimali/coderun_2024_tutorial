026f53587028b1a79928c06d5bef2b2eeb11893c
imimali <imimali18@gmail.com>
2024-11-11 21:49:26 +0200

    Add changes for last steps

--- demo/forms.py
+++ demo/forms.py
@@ -5,4 +5,4 @@ class UserDataForm(forms.Form):
     user_name = forms.CharField(label='Name', max_length=20)
     email = forms.EmailField(label='Email')
     weight = forms.IntegerField(label='Weight', min_value=0, max_value=300)
-    gender = forms.ChoiceField(label='Gender', choices=[(1,'male'), (2,'female')],widget=forms.RadioSelect())
+    gender = forms.ChoiceField(label='Gender', choices=[('male','male'), ('female','female')],widget=forms.RadioSelect())
--- demo/views.py
+++ demo/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from demo.forms import UserDataForm
+from demo.models import User
 
 
 def index(request):
@@ -14,4 +15,12 @@ def inspect_request(request):
 
 def register_user(request):
     user_data_form = UserDataForm()
-    return render(request,'demo/user.html',context=dict(form=user_data_form))
+    if request.method == "POST":
+        name = user_data_form.cleaned_data['user_name']
+        email = user_data_form.cleaned_data['email']
+        weight = user_data_form.cleaned_data['weight']
+        gender = user_data_form.cleaned_data['gender']
+        user = User(user_name=name, email=email, weight=weight, gender=gender)
+        user.save()
+        # TODO: querying the db on every request like this is very bad practice
+    return render(request,'demo/user.html',context=dict(form=user_data_form, users=User.objects.all()))
